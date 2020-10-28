85a4f0b71a09117eea4a2f1a3db8fc08be2b4c7b
Agnet-group <73598849+Agnet-group@users.noreply.github.com>
2020-10-28 22:27:42 +0200

    Initial release...

---
+++ upload/catalog/language/en-gb/extension/payment/kevin.php
@@ -0,0 +1,28 @@
+<?php
+/*
+* 2020 Kevin. payment  for OpenCart v.3.0.x.x  
+* @version 0.1.3.10
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+*  @author 2020 kevin. <info@getkevin.eu>
+*  @copyright kevin.
+*  @license http://opensource.org/licenses/afl-3.0.php Academic Free License (AFL 3.0)
+*/
+// Text
+$_['text_title']                           = 'Kevin';
+$_['text_order_info']                      = 'Order ID %s';
+
+//errors
+$_['error_currency']                       = 'Only &euro; currency are allowed for this payment method. Please select a currency <strong>&euro;</strong>.';
+$_['error_bank']                           = 'Bank not selected! Choose your bank.';
+$_['error_kevin_payment']                  = 'An error occurred. Please contact the merchant for more information, or try another payment method.';
+$_['error_kevin_payment_id']               = 'An error occurred. Payment ID was not returned from the payment system. Please contact the merchant for more information.';
